8a26c6779b8f7b6b8c657ac5c7c8304afd97db7a
Sayani-K <sayanikathir1010@gmail.com>
2025-09-18 10:44:23 +0530

    add edit for article

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,8 +1,26 @@
+// const express = require('express');
+// const router = express.Router();
+// const { signup, login } = require('../controllers/userController');
+
+// router.post('/signup', signup);
+// router.post('/login', login);
+
+// module.exports = router;
+
+
+
 const express = require('express');
 const router = express.Router();
-const { signup, login } = require('../controllers/userController');
+const {
+  signup, login, listUsers, getUserById, updateUser, deleteUser
+} = require('../controllers/userController');
 
 router.post('/signup', signup);
 router.post('/login', login);
+
+router.get('/users', listUsers);
+router.get('/users/:id', getUserById);
+router.put('/users/:id', updateUser);
+router.delete('/users/:id', deleteUser);
 
 module.exports = router;
